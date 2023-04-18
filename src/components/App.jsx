e20c937e5a62c1d9844e9c2d5ab69b33740df0f4
Stepan Skichko <st.skichko@gmail.com>
2023-04-18 11:47:44 +0300

    step 5 DONE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,20 +7,30 @@ import { nanoid } from 'nanoid';
 export class App extends Component {
   state = {
     contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
   formSubmitHandler = data => {
     const id = nanoid();
     const dataArr = { ...data, id };
+    console.log(dataArr);
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, dataArr],
-    }));
+    if (
+      this.state.contacts.some(
+        contact => contact.name.toLowerCase() === dataArr.name.toLowerCase()
+      )
+    ) {
+      alert(`${dataArr.name} is already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, dataArr],
+      }));
+    }
   };
 
   handleInputChange = evt => {
@@ -29,13 +39,10 @@ export class App extends Component {
 
   filter = () => {
     const { contacts, filter } = this.state;
-
-    // новий масив, який містить всі контакти, що містять рядок пошуку
     const filteredContacts = contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
 
-    // повернення нового масиву, який містить тільки ті контакти, які відповідають рядку пошуку
     return filteredContacts;
   };
 
@@ -45,7 +52,10 @@ export class App extends Component {
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
         <h2>Contacts</h2>
-        <Filter filter={this.state.filter} handleInputChange={this.handleInputChange}></Filter>
+        <Filter
+          filter={this.state.filter}
+          handleInputChange={this.handleInputChange}
+        ></Filter>
         <ContactList contacts={this.filter()}></ContactList>
       </div>
     );
--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
 export const List = styled.ul`
-// display: inline-flex;
-// flex-direction: column;
-// gap: 10px;
-// padding: 20px;
-
-// border: 1px solid gray;
-// border-radius: 6px;
 list-style: none;
+padding: 0;
 `;
